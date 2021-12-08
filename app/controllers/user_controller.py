27c77dabf66fc0ef009ba908aa3f430941336f4f
Divinity <systemdivinity@gmail.com>
2021-12-08 18:46:16 -0300

    allrigth

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,3 +1,7 @@
+from flask import request, current_app
+from flask.json import jsonify
+from werkzeug.exceptions import NotFound
+from app.models.user_model import UserModel
 def create_user():
         ...
 
@@ -6,11 +10,27 @@ def update_user():
         ...
 
 
+def get_user_by_id(id):
+        try:
+                
+                query = UserModel.query.filter_by(id=id).first_or_404()
+                return jsonify(query),200
+
+        except NotFound:
+                return {"msg": "User not Found"},404
+
 # discutir necessidade
 def get_user():
         ...
 
 
 # discutir necessidade
-def delete_user():
-        ...
+def delete_user(id):
+        try:
+                query = UserModel.filter_by(id=id).first_or_404()
+                current_app.db.session.delete(query)
+                current_app.db.session.commit()
+                return '', 204
+        except NotFound:
+                return {"msg": "User not Found"},404
+

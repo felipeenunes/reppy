1901c7ff0ff8c1c33cd606fa8f0b4b64eaf3a7aa
Natalia Nunes <naticristinenunes@gmail.com>
2021-12-08 15:43:28 -0300

    feat create user controller

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,5 +1,18 @@
+from flask import jsonify, request, current_app
+from app.models.adress_model import AdressModel
+from app.models.user_model import UserModel
+from app.models.state_model import StateModel
+
 def create_user():
-        ...
+    data = request.get_json()
+    data_adress = {key:value for key,value in data.items() if key == "adress"}
+    del data['adress']
+   # data['address_id'] = 
+    user = UserModel(**data)
+    current_app.db.session.add(user)
+    current_app.db.session.commit()
+
+
 
 
 def update_user():

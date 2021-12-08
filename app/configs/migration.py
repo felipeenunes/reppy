c14e5c6531f16f9c63f96669c094b959f74e698d
Gustavo  Prado Nunes <gustavo-pradogt@hotmail.com>
2021-12-08 18:34:46 +0000

    Revert "Merge branch 'insert_address' into 'master'"
    This reverts merge request !8

--- app/configs/migration.py
+++ app/configs/migration.py
@@ -3,11 +3,4 @@ from flask_migrate import Migrate
 
 
 def init_app(app: Flask):
-    from app.models.state_model import StateModel
-    from app.models.address_model import AddressModel
-    from app.models.user_model import UserModel
-    from app.models.republic_model import RepublicModel
-    from app.models.picture_model import PictureModel
-
-
     Migrate(app, app.db)
--- app/routes/republics_blueprint.py
+++ app/routes/republics_blueprint.py
@@ -1,11 +1,9 @@
 from flask import Blueprint
 
-from app.controllers.republics_controller import create_republic, get_all_republics
-
 bp = Blueprint("bp_republics", __name__, url_prefix='/republic')
 
-bp.post("")(create_republic)
-bp.get("")(get_all_republics)
+bp.post("")
+bp.get("")
 bp.patch("")
 bp.delete("")
 

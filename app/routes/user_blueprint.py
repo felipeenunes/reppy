f83af6997e599cf868b622e6cf9bcf9a406c9f4d
Priccila Lucem <systemdivinity@gmail.com>
2021-12-08 22:16:06 +0000

    Update app/routes/user_blueprint.py

--- app/routes/user_blueprint.py
+++ app/routes/user_blueprint.py
@@ -2,9 +2,9 @@ from flask import Blueprint
 from app.controllers.user_controller import (create_user,
  delete_user,
  get_user_by_id)
+
 bp = Blueprint("bp_user", __name__, url_prefix='/user')
 
-bp = Blueprint("bp_student", __name__, url_prefix='/student')
 bp.post("")(create_user)
 bp.get("<cpf>")(get_user_by_id)
 bp.delete("<cpf>")(delete_user)

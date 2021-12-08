c224a947353ee055161b90c8b64258702b3b5f1b
Gustavo P. Nunes <gustavo-pradogt@hotmail.com>
2021-12-08 11:25:57 -0300

    feat: add simple post and get

--- app/controllers/republics_controller.py
+++ app/controllers/republics_controller.py
@@ -1,13 +1,25 @@
+from flask import request
+from flask.json import jsonify
+from app.models.republic_model import RepublicModel
+from app.configs.database import db
+
 def create_republic():
-    ...
+    data = request.get_json()
+    
+    republic = RepublicModel(**data)
+    db.session.add(republic)
+    db.session.commit()
+
+
 
 
 def update_republic():
     ...
 
 
-def get_republic():
-    ...
+def get_all_republics():
+    republics = RepublicModel.query.all()
+    return jsonify(republics)
 
 
 def delete_republic():
--- app/routes/republics_blueprint.py
+++ app/routes/republics_blueprint.py
@@ -1,9 +1,11 @@
 from flask import Blueprint
 
+from app.controllers.republics_controller import create_republic, get_all_republics
+
 bp = Blueprint("bp_republics", __name__, url_prefix='/republic')
 
-bp.post("")
-bp.get("")
+bp.post("")(create_republic)
+bp.get("")(get_all_republics)
 bp.patch("")
 bp.delete("")
 

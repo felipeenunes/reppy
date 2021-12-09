375a54240374e42156dccf1eec8b3c647a14868b
Natalia Nunes <naticristinenunes@gmail.com>
2021-12-09 14:20:36 -0300

    fix:created login, and renamed password column

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 def init_app(app: Flask) -> None:
     from .user_blueprint import bp as bp_user
     from .republics_blueprint import bp as bp_republics
+    from.login_blueprint import bp as bp_login
 
     app.register_blueprint(bp_user)
     app.register_blueprint(bp_republics)
+    app.register_blueprint(bp_login)
---
+++ app/routes/login_blueprint.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+from app.controllers.user_controller import login_user
+
+bp = Blueprint("bp_login", __name__, url_prefix='/login')
+
+
+bp.post("")(login_user)

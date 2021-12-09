37d1282bfba13d69e64aa4390adcc9fda31d7cec
Natalia Nunes <naticristinenunes@gmail.com>
2021-12-08 22:01:32 -0300

    fix:creating except in user

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -1,34 +1,54 @@
 from flask import jsonify, request, current_app
 from app.models.user_model import UserModel
 from app.controllers.address_controller import create_address
+from app.exc.exc import PhoneError
+from sqlalchemy.exc import IntegrityError
+from psycopg2.errors import UniqueViolation
+from app.models.address_model import AddressModel
+
+def address_get(address):
+    address = AddressModel.query.get(address)
+    current_app.db.session.delete(address)
+    current_app.db.session.commit()
 
 def create_user():
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    data_address = data.pop("address")
-   
+        data_address = data.pop("address")
+        
+        data['address_id'] = create_address(data_address)
+        
+        user = UserModel(**data)
+        current_app.db.session.add(user)
+        current_app.db.session.commit()
+        
+        return jsonify(user), 201
+    except ValueError:
+        address_get(data['address_id'])
+        return {"error":"Field cpf must have 11 characters"},400
+    except PhoneError as err:
+        address_get(data['address_id'])
+        return jsonify({'Erro':str(err)}),400
+    except (UniqueViolation, IntegrityError):
+        current_app.db.session.rollback()
+        address_get(data['address_id'])
+        return jsonify({'Error':'cpf, email ou name already exists'}),409
     
-    data['address_id'] = create_address(data_address)
-    print(data)
 
-    user = UserModel(**data)
-    current_app.db.session.add(user)
-    current_app.db.session.commit()
-    
-    return jsonify(user), 201
 
 
 
 
 def update_user():
-        ...
+    ...
 
 
 # discutir necessidade
 def get_user():
-        ...
+    ...
 
 
 # discutir necessidade
 def delete_user():
-        ...
+    ...

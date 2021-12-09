f5159af7ee7eadad23ebda11fb52ded0920b17b3
Natalia Nunes <naticristinenunes@gmail.com>
2021-12-09 15:17:40 +0000

    Update database.py

--- app/configs/database.py
+++ app/configs/database.py
@@ -9,7 +9,4 @@ def init_app(app: Flask):
 
     app.db = db
 
-    # from app.models.task_model import TaskModel
-    # from app.models.eisenhower_model import EisenhowerModel
-    # from app.models.category_model import CategoryModel
-    # from app.models.tasks_categories_table import tasks_categories_table
+ 

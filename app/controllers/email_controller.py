6d52ca00ad36563d27f6639688f2d9db83f2c3b5
Gustavo P. Nunes <gustavo-pradogt@hotmail.com>
2021-12-13 18:32:49 -0300

    fix: email sensitive infos is now on .env

--- app/controllers/email_controller.py
+++ app/controllers/email_controller.py
@@ -1,27 +1,36 @@
+from os import environ
 from flask import request
 import smtplib
 import email.message
+from smtplib import SMTPRecipientsRefused
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def send_email():
 
-    data = request.get_json()
+    try:
+        data = request.get_json()
 
-    print(data["message"])
+        print(data["message"])
 
-    msg =   data["message"]
+        msg =   data["message"]
 
 
-    mail = email.message.Message()
-    mail["Subject"] = "outro email"
-    mail["From"] = 'contatoreppy@gmail.com'
-    password = '@Senha123'
-    mail["To"] = data["recipient"]
-    mail.add_header("Content_Type", "text/html")
-    mail.set_payload(msg)
+        mail = email.message.Message()
+        mail["Subject"] = data["subject"]
+        mail["From"] = environ.get("EMAIL")
+        password = environ.get("EMAIL_PASSWORD")
+        mail["To"] = data["recipient"]
+        mail.add_header("Content_Type", "text/html")
+        mail.set_payload(msg)
 
-    server = smtplib.SMTP("smtp.gmail.com:587")
-    server.starttls()
-    server.login(mail["From"], password)
-    server.sendmail(mail["From"], [mail["To"]], mail.as_string().encode("utf-8"))
-    print("email enviado!")
+        server = smtplib.SMTP(environ.get("SMTP_SERVER"))
+        server.starttls()
+        server.login(mail["From"], password)
+        server.sendmail(mail["From"], [mail["To"]], mail.as_string().encode("utf-8"))
+        return {"msg": "email sent"}
+    except SMTPRecipientsRefused:
+        return {"msg": "error when sending email"}, 400
+
+    

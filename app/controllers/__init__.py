e7d67cede842a0ff27f1ba2a75da2c764dd27016
Natalia Nunes <naticristinenunes@gmail.com>
2021-12-16 13:02:16 +0000

    Update app/controllers/__init__.py

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -14,7 +14,7 @@ def verification(data,keys):
             if not key in data:
                 missing_keys.append(key)
             if (key in data) and (value != type(data[key])):
-                print(value,"macarrão", type(data[key]))
+               
                 value_incorrect.append({"value correct":{key:value}, "past value":{key:type(data[key])}})
     if missing_keys: raise BadRequestError(f'Required keys not found: {missing_keys}')
     if value_incorrect: raise BadRequestError(f'Required correct values: {value_incorrect}')

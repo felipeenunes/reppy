17551bc7e37a029af16877afc0916c73023515a9
Gustavo P. Nunes <gustavo-pradogt@hotmail.com>
2021-12-16 13:07:04 -0300

    fix: data type errors

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -15,12 +15,10 @@ def verification(data,keys):
                 missing_keys.append(key)
             if (key in data) and (value != type(data[key])):
                 value = str(value).replace("<class ", "").replace(">", "")
-                # value_incorrect.append(f"correct type for {key}:{value}")
                 inserted_type = type(data[key])
                 inserted_type = str(inserted_type).replace("<class", "").replace(">", "")
-                raise BadRequestError(f"Invalid type for {key}. Inserted type: {inserted_type}, correct type: {value} ")
-                print(inserted_type)
+                raise BadRequestError(f"""Invalid type for {key}. 
+                Inserted type: {inserted_type}, correct type: {value} """)
     if missing_keys: raise BadRequestError(f'Required keys not found: {missing_keys}')
     if value_incorrect: raise BadRequestError(f'Invalid data type: {value_incorrect}')
 
-#  "past value":{key:type(data[key])}}

a3ae91e1b8fbe294797f59180ae633cc49c66123
Gustavo P. Nunes <gustavo-pradogt@hotmail.com>
2021-12-16 12:12:39 -0300

    fix json

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -14,7 +14,9 @@ def verification(data,keys):
             if not key in data:
                 missing_keys.append(key)
             if (key in data) and (value != type(data[key])):
-               
-                value_incorrect.append({"value correct":{key:value}, "past value":{key:type(data[key])}})
+                value = str(value)
+                teste = value.replace("class ", "").replace("<", "").replace(">", "")
+                print(teste)
+                value_incorrect.append(f"correct value for {key} : {teste}")
     if missing_keys: raise BadRequestError(f'Required keys not found: {missing_keys}')
     if value_incorrect: raise BadRequestError(f'Required correct values: {value_incorrect}')
